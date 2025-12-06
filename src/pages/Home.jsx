a3d38e846e87a909b5afafefc6aa74eee1f93fb5
haifazahirahr <haifazahirahr5@gmail.com>
2025-12-06 17:20:09 +0700

    menambahkan section Features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import CatalogPlants from "../components/CatalogPlant";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Features from "../components/Features";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Features />
 
       <CatalogPlants />
     </>
