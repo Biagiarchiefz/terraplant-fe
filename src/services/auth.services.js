adc5936b3c448c072f92928d0e2cd7d0799f169d
Biagiarchiefz <biagiiarchie@gmail.com>
2025-12-30 14:27:30 +0700

    feat: add login google

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,33 +1,37 @@
 import api from "./api";
 
 export const register = async (payload) => {
-    try {
-        const response = await api.post("/auth/register", payload);
-        return response;
-    } catch (error) {
-        console.log(error.message);
-        throw error;
-    }
+  try {
+    const response = await api.post("/auth/register", payload);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
 };
 
 export const login = async (payload) => {
-    try {
-        const response = await api.post("/auth/login", payload);
-        return response;
-    } catch (error) {
-        //  toast.error("Semothing went wrong");
-        console.log(error.message);
-        throw error;
-    }
+  try {
+    const response = await api.post("/auth/login", payload);
+    return response;
+  } catch (error) {
+    //  toast.error("Semothing went wrong");
+    console.log(error.message);
+    throw error;
+  }
 };
 
 export const me = async () => {
-    try {
-        const response = await api.get("/auth/me");
-        return response;
-    } catch (error) {
-        //  toast.error("Semothing went wrong");
-        console.log(error.message);
-        throw error;
-    }
+  try {
+    const response = await api.get("/auth/me");
+    return response;
+  } catch (error) {
+    //  toast.error("Semothing went wrong");
+    console.log(error.message);
+    throw error;
+  }
+};
+
+export const googleLogin = async (token) => {
+  return await api.post("/auth/google-login", { token });
 };
